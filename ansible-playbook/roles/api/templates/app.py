e7318622110f5f3b2a716cec7958096b1d9274fe
ALEXGRISHIN1405 <104369794+ALEXGRISHIN1405@users.noreply.github.com>
2024-10-28 21:37:39 +0300

    task 1

--- ansible-playbook/roles/api/templates/app.py
+++ ansible-playbook/roles/api/templates/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request # Импорт необходимых библиотек
 import psycopg2
 
-app = Flask(__name__)
+app = Flask(__name__) # Создание экземпляра приложения
 
-def get_db_connection():
+def get_db_connection(): # Функция для подключения к базе данных
     conn = psycopg2.connect(host='localhost', database='test', user='postgres', password='postgres')
     return conn
 
-@app.route('/api/data', methods=['GET'])
+@app.route('/api/data', methods=['GET']) # Определение маршрута для получения данных
 def get_data():
     conn = get_db_connection()
     cur = conn.cursor()
@@ -17,5 +17,5 @@ def get_data():
     conn.close()
     return jsonify(rows)
 
-if __name__ == '__main__':
+if __name__ == '__main__': # Запуск приложения
     app.run(host='0.0.0.0', port=5000)
